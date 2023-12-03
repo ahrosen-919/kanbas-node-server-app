d960f2c755591f3b78cf927ba17819012fdcd65b
Aidan Rosenberg <aidan@rosenbergs.org>
2023-12-03 17:56:17 -0500

    a6-sections 2 + 3

--- app.js
+++ app.js
@@ -3,15 +3,19 @@ import express from 'express';
 import Hello from "./hello.js";
 import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from "./modules/routes.js";
+import UserRoutes from "./users/routes.js";
 import Lab5 from "./Lab5.js";
 import cors from "cors";
+import mongoose from "mongoose";
+mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
 
 const app = express()
 app.use(cors());
 
 app.use(express.json());
+UserRoutes(app);
 Hello(app);
 Lab5(app);
 ModuleRoutes(app);
 CourseRoutes(app)
-app.listen(process.env.PORT || 4000)
+app.listen(4000);
